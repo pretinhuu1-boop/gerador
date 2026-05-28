d89d69369795e935cfd9c87de426f6925f633e8e
nelson <pretinhuu1@gmail.com>
2026-05-28 06:53:49 -0300

    feat(crew-running): creator FotoTab + PerfilTab + LookTab shells

---
+++ apps/crew-running/components/creator/tabs/PerfilTab.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { RunnerProfileForm } from '../../RunnerProfileForm';
+import type { RunnerProfile } from '../../../data/runnerProfile';
+
+type Props = {
+  profile: RunnerProfile;
+  onChange: (profile: RunnerProfile) => void;
+};
+
+export const PerfilTab: React.FC<Props> = ({ profile, onChange }) => (
+  <section role="tabpanel" aria-labelledby="creator-tab-perfil" className="runner-tab__section">
+    <RunnerProfileForm profile={profile} onChange={onChange} />
+  </section>
+);
